20107cae618bbe52985c9f183132e77c1258fed1
mbuhendwa <mkbuhendwa@gmail.com>
2022-09-21 15:28:27 +0100

    fix: remove type submit on link

--- src/components/Elements/ButtonLink.tsx
+++ src/components/Elements/ButtonLink.tsx
@@ -11,15 +11,14 @@ interface Props {
 export const ButtonLink: FC<Props> = ({ classNames, label, linkTo }) => {
   return (
     <a href={linkTo}>
-      <button
-        type="submit"
+      <span
         className={classNamesConcat(
           classNames?.button ||
             'inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500'
         )}
       >
         {label || 'Submit'}
-      </button>
+      </span>
     </a>
   );
 };
